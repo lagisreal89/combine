d01f04c5d125aab94658871fe599c4584e9fbeba
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-05-04 21:37:58 -0400

    added comments

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-import nfl_data_py as nfl 
+import nfl_data_py as nfl #web scrapping for the combine data from 2000-2024
 import pandas as pd
 import sklearn
 from sklearn import tree
@@ -14,7 +14,7 @@ import numpy as np
 scraped_data = nfl.import_combine_data([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023])
 scraped_data = scraped_data[['draft_year', 'draft_ovr', 'player_name','ht','wt','forty','bench','vertical','broad_jump']]
 print(scraped_data.info())
-draft = []
+draft = [] # tell whether or not someone was drafted (target value)
 for row in scraped_data.itertuples(index=True):
    if (np.isnan(row.draft_ovr)):
         draft.append(False)
