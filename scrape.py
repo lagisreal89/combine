2b2ac2a9b66d8171f1b189c991a07b9cdddf642b
lagisreal89 <56201840+lagisreal89@users.noreply.github.com>
2024-05-09 23:03:09 -0400

    added boosters and svm stuff

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sklearn
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import metrics
 from sklearn.tree import export_graphviz
@@ -14,6 +14,7 @@ import re
 import numpy as np
 from sklearn.datasets import load_iris
 import pydot
+import sklearn.svm as svm
 scraped_data = pd.DataFrame(nfl.import_combine_data([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023]))
 print(scraped_data)
 scraped_data = scraped_data[['draft_year', 'draft_ovr', 'player_name','ht','wt','forty','bench','vertical','broad_jump','cone','shuttle']]
@@ -24,8 +25,6 @@ for row in scraped_data.itertuples(index=True):
         draft.append(False)
    else :
         draft.append(True)
-for row in range(len(scraped_data['ht'])):
-     height_str = scraped_data['ht'].get(row)
 scraped_data['drafted'] = draft
 print(scraped_data)
 X = scraped_data.iloc[:,5:11]
@@ -39,7 +38,10 @@ clf = DecisionTreeClassifier()
 print(cross_val_score(clf, X_train, y_train, cv=7))
 clf.fit(X_train, y_train)
 print(metrics.accuracy_score(y_valid, clf.predict(X_valid)))
-print(clf.feature_importances_)
+svm_cross = svm.SVC(kernel='linear', C=1)
+print(cross_val_score(svm_cross,X_train, y_train, cv=7))
+svm_cross.fit(X_train, y_train)
+print(metrics.accuracy_score(y_valid, svm_cross.predict(X_valid)))
 def sortSecond(val):
      return val[1]
 values = clf.feature_importances_
@@ -74,29 +76,29 @@ export_graphviz(
      rounded=True,
      filled=True
 )
-# start of adaboost code
-clf = AdaBoostClassifier(algorithm="SAMME", random_state=0)
+# start of bagging code
+clf = BaggingClassifier(random_state=0)
 print(cross_val_score(clf, X_train, y_train, cv=10))
 clf.fit(X_train, y_train)
 print(metrics.accuracy_score(y_valid, clf.predict(X_valid)))
 params_dist = {
-     'algorithm' : ['SAMME'],
-     'n_estimators' : randint(low=1, high=100),
-     'learning_rate': randint(low=1,high=25)
+     'n_estimators':randint(low=1,high=100),
+     'max_samples':[x / 10 for x in range(1, 11)],
+     'max_features':[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.90, 0.92, 0.95, 1.0]
 }
-clf_tuned = AdaBoostClassifier(random_state = 42)
+clf_tuned = BaggingClassifier(random_state=42)
 random_search = RandomizedSearchCV(clf_tuned, params_dist, cv=7)
 random_search.fit(X_train, y_train)
 print(random_search.best_estimator_)
 best_tuned_clf = random_search.best_estimator_
 print(metrics.accuracy_score(y_valid, best_tuned_clf.predict(X_valid)))
-export_graphviz(
-     best_tuned_clf.estimators_[0],
-     out_file='adaboosttree.dot',
+'''export_graphviz(
+     best_tuned_clf,
+     out_file='bagging.dot',
      feature_names=list(X_train),
      rounded=True,
      filled=True
-)
+)'''
 # randomforest booster code
 clf = RandomForestClassifier()
 print(cross_val_score(clf, X_train, y_train, cv=10))
